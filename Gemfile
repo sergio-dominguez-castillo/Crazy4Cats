475d5ff0529a461817bd07ed3a4a66d56106a359
Sergio Dominguez <sergio.castle@gmail.com>
2023-11-02 01:51:56 -0300

    Feat: configuraciones de reactions

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
 gem "devise", "~> 4.9"
+
+gem "faker", "~> 3.2"
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -2,4 +2,7 @@ class Publication < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :reactions, dependent: :destroy
     has_many :users, through: :reactions
+
+    Kinds = %w[like dislike].freeze
+    KindsSpanish = {"like" => "Me gusta", "dislike" => "No me gusta"}.freeze
 end
